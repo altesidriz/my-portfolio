f95396e3863e0e1a2a8081b05a9a69c48079cfe8
Idriz Altes <99037664+LetRookie@users.noreply.github.com>
2024-09-15 09:12:18 +0300

    init project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./app.scss"
+
+const App = () => {
+  return <div>
+    <section>Hero</section>
+    <section>Parallax</section>
+    <section>Project1</section>
+    <section>Project2</section>
+    <section>Project3</section>
+    <section>Project4</section>
+    <section>Info</section>
+  </div>;
+};
+
+export default App;
