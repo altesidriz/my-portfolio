09d0cd1a3dddd39914aa6f6095f57bbd9d54291c
Idriz Altes <99037664+LetRookie@users.noreply.github.com>
2024-10-15 20:35:35 +0300

    update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,20 +1,37 @@
-import React from 'react'
+import Sidebar from "../sidebar/Sidebar";
 import "./navbar.scss";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
     <div className="navbar">
-        {/* Sidebar */}
-        <div className="wrapper">
-            <span>LetRookie</span>
-            <div className="social">
-                <a href="#" className="link"><img src="/facebook.png" alt="" /></a>
-                <a href="#" className="link"><img src="/instagram.png" alt="" /></a>
-                <a href="#" className="link"><img src="/facebook.png" alt="" /></a>
-            </div>
+      {/* Sidebar */}
+      <Sidebar/>
+      <div className="wrapper">
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          Lama Dev
+        </motion.span>
+        <div className="social">
+          <a href="#">
+            <img src="/facebook.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/instagram.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/youtube.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/dribbble.png" alt="" />
+          </a>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
---
+++ src/components/navbar/Test.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <div className='course'>
+        <div className="box">
+
+        </div>
+    </div>
+  )
+}
+
+export default Test
